c1ac80b12fc27179154d2bbfb9f71d8a4b6a4822
Trixie Roque <troque@lion.lmu.edu>
2015-11-03 19:42:17 -0800

    Would now print blog title

--- front-end/front-end.js
+++ front-end/front-end.js
@@ -1,15 +1,47 @@
 $(function () {
 
+	key = "1yk3GzGkBaDRoW7niKqMk00xyIPBJ8UccF0zrmvcVk95fHNqJv";
+	title = "";
+
 	$("#search-button").click(function () {
-		$.getJSON (
-                           "http://localhost:3000/v2/blog/" + $("#search-term").val() + "/info", 
+		searchBlog();
+		printBlogTitle();
+	});
+
+	$('#search-button').keypress (function (e) {
+		if (e.which == '13' || e.keyCode == '13') {
+			e.preventDefault();
+			searchBlog();
+		}
+	});
+
+	$('#search-button').on ('keypress', function (event) {
+		if (e.keyCode == 13) {
+			searchBlog();
+		}
+		
+	})
+});
+
+function printBlogTitle (result) {
+	$('#text-output').text (title);
+}
+
+function searchBlog () {
+	$.getJSON (
+            "http://localhost:3000/v2/blog/" + $("#search-term").val() + ".tumblr.com" + "/info", 
 			{
-				// base-hostname: $("#search-term").val(),
-				api_key: "1yk3GzGkBaDRoW7niKqMk00xyIPBJ8UccF0zrmvcVk95fHNqJv"
+				api_key: key
 			}
 		).done(function (result) {
 			console.log(result);
+			console.log(result.response.blog.title);
+			title = result.response.blog.title;
+			if (!result.response.blog.ask_anon) {
+				console.log ("OK");
+			}
+			// if (result.meta.status === 404) {
+			// 	alert ("User does not exist!");
+			// }
 		});
-	});
-
-});
+}
