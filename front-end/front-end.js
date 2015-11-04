8f1dfc6ab92f0cf5168d1089fd927754f0d96176
Trixie Roque <troque@lion.lmu.edu>
2015-11-03 20:46:24 -0800

    Added some more options

--- front-end/front-end.js
+++ front-end/front-end.js
@@ -1,11 +1,17 @@
 $(function () {
 
 	key = "1yk3GzGkBaDRoW7niKqMk00xyIPBJ8UccF0zrmvcVk95fHNqJv";
-	title = "";
+	var title = "";
+	var likes = "";
+	var posts = "";
+	var name = "";
+	var description = "";
 
 	$("#search-button").click(function () {
-		searchBlog();
-		printBlogTitle();
+		// searchBlog();
+		// printBlogTitle();
+		getLikes();
+		getPosts();
 	});
 
 	$('#search-button').keypress (function (e) {
@@ -15,28 +21,39 @@ $(function () {
 		}
 	});
 
-	$('#search-button').on ('keypress', function (event) {
-		if (e.keyCode == 13) {
-			searchBlog();
-		}
-		
-	})
-});
-
-function printBlogTitle (result) {
-	$('#text-output').text (title);
+	function printBlogTitle () {
+	$('#blog-title').text (title);
 }
 
-function searchBlog () {
-	$.getJSON (
+// function printProperties () {
+	
+// 	if ($('#name').attr('checked')) {
+// 		$('#blog-details').text(name);
+// 	}
+// 	if ($('#likes').attr('checked')) {
+// 		$('#blog-details').text(likes);
+// 	}
+// 	if ($('#posts').attr('checked')) {
+// 		$('#blog-details').text(posts);
+// 	}
+// 	if ($('#description').attr('checked')) {
+// 		$('#blog-details').text(description);
+// 	}
+// }
+
+	function searchBlog() {
+		$.getJSON (
             "http://localhost:3000/v2/blog/" + $("#search-term").val() + ".tumblr.com" + "/info", 
 			{
 				api_key: key
 			}
 		).done(function (result) {
 			console.log(result);
-			console.log(result.response.blog.title);
 			title = result.response.blog.title;
+			name = result.response.blog.name;
+			likes = result.response.blog.likes;
+			posts = result.response.blog.posts;
+			description = result.response.blog.description;
 			if (!result.response.blog.ask_anon) {
 				console.log ("OK");
 			}
@@ -44,4 +61,34 @@ function searchBlog () {
 			// 	alert ("User does not exist!");
 			// }
 		});
-}
+	}
+
+	function getLikes() {
+		$.getJSON (
+			"http://localhost:3000/v2/blog/" + $("#search-term").val() + ".tumblr.com" + "/likes",
+			{
+				api_key: key
+			}
+		).done(function (result) {
+			console.log(result);
+			likes = result;
+		})
+
+	}
+
+	function getPosts() {
+		$.getJSON (
+			"http://localhost:3000/v2/blog/" + $("#search-term").val() + ".tumblr.com" + "/posts",
+			{
+				api_key: key
+			}
+		).done(function (result) {
+			console.log(result);
+
+			$('body').append(result);
+		})
+	}
+
+});
+
+
