ec2249bf32187cf68beb2e7713593bf7d61588bd
Trixie Roque <troque@lion.lmu.edu>
2015-11-12 10:44:02 -0800

    Added boxes-with-physics to cmsi370

---
+++ boxes-with-physics/boxes-touch.js
@@ -0,0 +1,89 @@
+(function ($) {
+    /**
+     * Tracks a box as it is rubberbanded or moved across the drawing area.
+     */
+    var trackDrag = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            // Don't bother if we aren't tracking anything.
+            if (touch.target.movingBox) {
+                // Reposition the object.
+                touch.target.movingBox.offset({
+                    left: touch.pageX - touch.target.deltaX,
+                    top: touch.pageY - touch.target.deltaY
+                });
+            }
+        });
+
+        // Don't do any touch scrolling.
+        event.preventDefault();
+    };
+
+    /**
+     * Concludes a drawing or moving sequence.
+     */
+    var endDrag = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            if (touch.target.movingBox) {
+                // Change state to "not-moving-anything" by clearing out
+                // touch.target.movingBox.
+                touch.target.movingBox = null;
+            }
+        });
+    };
+
+    /**
+     * Indicates that an element is unhighlighted.
+     */
+    var unhighlight = function () {
+        $(this).removeClass("box-highlight");
+    };
+
+    /**
+     * Begins a box move sequence.
+     */
+    var startMove = function (event) {
+        $.each(event.changedTouches, function (index, touch) {
+            // Highlight the element.
+            $(touch.target).addClass("box-highlight");
+
+            // Take note of the box's current (global) location.
+            var jThis = $(touch.target),
+                startOffset = jThis.offset();
+
+            // Set the drawing area's state to indicate that it is
+            // in the middle of a move.
+            touch.target.movingBox = jThis;
+            touch.target.deltaX = touch.pageX - startOffset.left;
+            touch.target.deltaY = touch.pageY - startOffset.top;
+        });
+
+        // Eat up the event so that the drawing area does not
+        // deal with it.
+        event.stopPropagation();
+    };
+
+    /**
+     * Sets up the given jQuery collection as the drawing area(s).
+     */
+    var setDrawingArea = function (jQueryElements) {
+        // Set up any pre-existing box elements for touch behavior.
+        jQueryElements
+            .addClass("drawing-area")
+            
+            // Event handler setup must be low-level because jQuery
+            // doesn't relay touch-specific event properties.
+            .each(function (index, element) {
+                element.addEventListener("touchmove", trackDrag, false);
+                element.addEventListener("touchend", endDrag, false);
+            })
+
+            .find("div.box").each(function (index, element) {
+                element.addEventListener("touchstart", startMove, false);
+                element.addEventListener("touchend", unhighlight, false);
+            });
+    };
+
+    $.fn.boxesTouch = function () {
+        setDrawingArea(this);
+    };
+}(jQuery));
