73b4886681aba1e984a68190129f7e3ca0d732c1
Trixie Roque <troque@lion.lmu.edu>
2015-12-13 08:15:29 -0800

    Tweaking js and css files

--- widget-from-scratch/js/plugin.js
+++ widget-from-scratch/js/plugin.js
@@ -1,5 +1,25 @@
 (function ($) {
-	$.fn.dimmer = function () {
-		
-	}
+    $.fn.dimmer = function () {
+        $("body").append($("<div/>", {
+            id: "slider-container",
+            class: "black-overlay white-overlay"
+        }));
+        $("#slider-container").append($("<div/>", {
+            class: "slider"
+        }));
+
+        trackSlide();
+    }
+
+    var trackSlide = function (event) {
+        $(".slider").mouseover(function () {
+            $(".slider").css("background-color", "gray");
+        })
+        $(".slider").mouseout(function () {
+            $(".slider").css("background-color", "white");
+        })
+    }
+
+    $("#slider-container").dimmer();
+
 }) (jQuery);
