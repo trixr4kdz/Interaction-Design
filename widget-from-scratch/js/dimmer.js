88a0e9606d8cdfc7a5dea12064527973bef3f0dd
Trixie Roque <troque@lion.lmu.edu>
2015-12-14 03:25:08 -0800

    Fixed bug on going over slider-main

--- widget-from-scratch/js/dimmer.js
+++ widget-from-scratch/js/dimmer.js
@@ -5,18 +5,19 @@
 
         // }
         // $.extend(options, defaultProperties);
+        var clicking = false;
         setSliderArea(this);
         this
             .find("div.slider")
             .each(function () {
                 $(this)
-                    // .mousedown(slideDrag)
                     .mousedown(startDrag)
                     .mouseover(mouseIsOver)
                     .mouseup(endDrag)
+                // if (clicking) {
+                //     $(this).mousemove(trackDrag);
+                // }
             })
-        offset = clampSlider($("div.slider").offset(), this);
-        $(this).offset(offset);
         console.log("MAIN " + ($(".slider-main").offset().top + $(".slider-main").height()));
         console.log("SLIDER " + $(".slider").offset().top + $(".slider").height());
     }
@@ -45,87 +46,65 @@
     }
 
     var startDrag = function (event) {
+        clicking = true;
         var jThis = $(this),
             startOffset = jThis.offset();
         this.deltaY = event.pageY - startOffset.top;
-        $(this).mousemove(trackDrag);
+        if (clicking) {
+            $(this).mousemove(trackDrag);
+        }
         console.log(this.deltaY);
     }
 
     var trackDrag = function (event) {
-        $(this).offset({
-            top: event.pageY - this.deltaY
-        })
-        $()
-        console.log("event.pageY " + event.clientY);
-        console.log("offset " + $(this).offset().top)
+        var newTop = event.pageY - this.deltaY;
+        $(this).css("background", "gray");
+        var offset = clampSlider ({
+            top: newTop
+            }, 
+            this
+        )
+        console.log("offset " + offset.top)
+        console.log("event.pageY " + event.pageY);
+        // console.log("offset " + $(this).offset().top)
+
+        $(this).offset(offset);
+        console.log($(this).offset().top);
     }
 
     var endDrag = function (event) {
+        clicking = false;
+        console.log("endDrag")
         var lastY = event.pageY;
-
-            $(this).offset({
-                top: lastY
-            })
-            .mouseup(slideCleanUp)
-        
-        console.log("lastY " + lastY);
-
+        // $(this).offset({
+        //     top: lastY
+        // })
+        // .mouseup(slideCleanUp)
     }
 
-    var slideCleanUp = function (event) {
-         $(this)
-            .offset({
-                top: event.clientY
-            })
-            .unbind("mousemove", startDrag)
-            .css("background", "white");
-    }
+    // var slideCleanUp = function (event) {
+    //      $(this)
+    //         .offset({
+    //             top: event.clientY
+    //         })
+    //         .unbind("mousemove", startDrag)
+    //         .css("background", "white");
+    // }
 
     var clampSlider = function (offset, element) {
         var containerTop = $(".slider-main").offset().top;
         var containerBottom = containerTop + $(".slider-main").height();
         var sliderTop = offset.top;
-        var sliderBottom = sliderTop+ $(element).height();
-        offset.top = sliderTop < containerTop 
-                        ? containerTop 
-                        : sliderTop;
-        offset.top = sliderBottom > containerBottom
-                        ? containerBottom - sliderBottom
-                        : sliderTop
-        console.log(containerBottom);
+        var sliderBottom = sliderTop + $(element).height();
+
+        if (sliderTop < containerTop) {
+            offset.top = containerTop;
+        } else if (sliderBottom > containerBottom) {
+            offset.top = containerBottom - $(element).height();
+        } else {
+            offset.top = sliderTop;
+        }
         return offset;
     }
-
-    // var trackSlide = function (target) {
-    //     var isSliding = false;
-    //     $(target)
-    //         .mouseover(function () {
-    //             $(this)
-    //                 .css("background-color", "gray")
-    //                 .mousedown(function () {
-    //                     isSliding = false;
-    //                     $(this).css({
-    //                         background: "yellow"
-    //                     })
-    //                 .mousemove(function () {
-    //                     isSliding = true;
-    //                 })
-    //             })
-    //         })
-    //         .mouseup(function () {
-    //             var wasSliding = isSliding;
-    //             isSliding = false;
-    //             if (wasSliding) {
-    //                 $(target).css("background-color", "gray");
-    //             }
-    //         })
-    //         .mouseout(function () {
-    //             // if (!isSliding) {
-    //                 $(target).css("background-color", "white");
-    //             // }
-    //         })
-    // }
-
 
 }) (jQuery);
